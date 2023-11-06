307593c9214a7b66d27afe1cfeca35c5e28b4abb
mahmoudbalbaa <ma7moud.balbaa@gmail.com>
2023-11-06 13:14:51 -0800

    Can you C me now?

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    if isinstance(my_string, str):
+        nw_string = list(my_string)
+        for i in range(len(nw_string)):
+            if nw_string[i] == 'c' or nw_string[i] == 'C':
+                nw_string[i] = '' 
+        return ''.join(nw_string)
