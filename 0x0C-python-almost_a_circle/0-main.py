108eee7ff59ab5dcd8f62cd8496bbee1f6b9cbf3
Mahmoud Balbaa <138934815+mahmoudbalbaa@users.noreply.github.com>
2023-12-12 00:31:48 +0200

    Update 0-main.py

--- 0x0C-python-almost_a_circle/0-main.py
+++ 0x0C-python-almost_a_circle/0-main.py
@@ -4,17 +4,17 @@ from models.base import Base
 
 if __name__ == "__main__":
 
-    b1 = Base()
-    print(b1.id)
+    bs1 = Base()
+    print(bs1.id)
 
-    b2 = Base()
-    print(b2.id)
+    bs2 = Base()
+    print(bs2.id)
 
-    b3 = Base()
-    print(b3.id)
+    bs3 = Base()
+    print(bs3.id)
 
-    b4 = Base(12)
-    print(b4.id)
+    bs4 = Base(12)
+    print(bs4.id)
 
-    b5 = Base()
-    print(b5.id)
+    bs5 = Base()
+    print(bs5.id)
